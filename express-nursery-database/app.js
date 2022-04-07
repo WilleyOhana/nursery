17e7bc5d33aa4755ac30a61cc4a2cd2462fe925b
Willey <richardwohana@gmail.com>
2022-04-07 13:42:12 -0700

    Add CORS

--- express-nursery-database/app.js
+++ express-nursery-database/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const plantsRouter = require('./routes/api/plants');
@@ -8,13 +9,9 @@ const app = express();
 
 const PORT = 3001 || process.env.PORT;
 
-// Send React app, use CORS,
+// Send React app, use CORS
 app.use(express.static(path.join(__dirname, "..", "react-nursery-database", "build")));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.use("/plants", plantsRouter);
 
