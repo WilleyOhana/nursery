02a21fd1ff7655f14ee777900d7a2b0d0c20998c
Willey <richardwohana@gmail.com>
2022-04-09 17:52:39 -0700

    Add form to /add-item route

--- react-nursery-database/src/components/AddItem/AddItem.js
+++ react-nursery-database/src/components/AddItem/AddItem.js
@@ -1,9 +1,31 @@
 import './AddItem.css';
+import { GiTreeBranch } from 'react-icons/gi';
 
 const AddItem = () => {
+
+    const submitAddItemForm = (e) => {
+        e.preventDefault();
+
+
+    }
+
     return (
         <div className="add-item">
+            <form onSubmit={(e) => submitAddItemForm(e)}>
+                <GiTreeBranch className='icon' />
+                <h3>Add New Plant</h3>
 
+                <label htmlFor="name">Name of plant:</label>
+                <input name="name" type="text" id="name" placeholder="Name" />
+
+                <label htmlFor="quantity">Quantity on hand</label>
+                <input name="quantity" type="number" id="quantity" placeholder="Quantity" />
+
+                <label htmlFor="details">Details</label>
+                <textarea rows="5" placeholder='e.g. 2 gallon pot, requires loose soil and daily water' />
+
+                <input type="submit" />
+            </form>
         </div>
     )
 }
