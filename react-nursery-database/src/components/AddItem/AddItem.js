c1ae523eea88aac8370c27e75b7db743258fa043
Willey <richardwohana@gmail.com>
2022-04-10 07:38:59 -0700

    Add state, navigation, and proper handleSubmit function

--- react-nursery-database/src/components/AddItem/AddItem.js
+++ react-nursery-database/src/components/AddItem/AddItem.js
@@ -1,12 +1,22 @@
 import './AddItem.css';
+import axios from 'axios';
 import { GiTreeBranch } from 'react-icons/gi';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AddItem = () => {
 
-    const submitAddItemForm = (e) => {
+    const [name, setName] = useState('');
+    const [quantity, setQuantity] = useState(0);
+    const [details, setDetails] = useState('');
+
+    const navigate = useNavigate();
+
+    const submitAddItemForm = async (e) => {
         e.preventDefault();
 
-
+        await axios.post(`http://localhost:3001/plants/${name}/${quantity}/${details}/MarinnCarpenter`);
+        navigate('/');
     }
 
     return (
@@ -16,13 +26,35 @@ const AddItem = () => {
                 <h3>Add New Plant</h3>
 
                 <label htmlFor="name">Name of plant:</label>
-                <input name="name" type="text" id="name" placeholder="Name" />
+                <input 
+                    name="name" 
+                    type="text" 
+                    id="name" 
+                    placeholder="Name" 
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    required
+                />
 
                 <label htmlFor="quantity">Quantity on hand</label>
-                <input name="quantity" type="number" id="quantity" placeholder="Quantity" />
+                <input 
+                    name="quantity" 
+                    type="number" 
+                    id="quantity" 
+                    placeholder="Quantity"
+                    onChange={(e) => setQuantity(e.target.value)}
+                    value={quantity}
+                    required
+                />
 
                 <label htmlFor="details">Details</label>
-                <textarea rows="5" placeholder='e.g. 2 gallon pot, requires loose soil and daily water' />
+                <textarea 
+                    rows="5" 
+                    placeholder='e.g. 2 gallon pot, requires loose soil and daily water' 
+                    onChange={(e) => setDetails(e.target.value)}
+                    value={details}
+                    required
+                />
 
                 <input type="submit" />
             </form>
